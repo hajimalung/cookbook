3b47d2216b57fe17302329bdf5e1556ba662993f
hajimalung <hajimalungbaba.786@gmail.com>
2022-02-21 12:03:06 +0530

    recipes added

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -7,7 +7,23 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
-  recipes: Recipe[] = [];
+  recipes: Recipe[] = [
+    new Recipe(
+      'test reciepe 1',
+      'this is a test recipe described',
+      'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'
+    ),
+    new Recipe(
+      'test reciepe 2',
+      'this is a test recipe described',
+      'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'
+    ),
+    new Recipe(
+      'test reciepe 3',
+      'this is a test recipe described',
+      'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'
+    ),
+  ];
   constructor() {}
 
   ngOnInit(): void {}
