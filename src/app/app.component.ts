24d3f7a39fcb16a784332f0a9369545e280665ca
hajimalung <hajimalungbaba.786@gmail.com>
2022-02-23 17:09:55 +0530

    add simple navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,4 +5,9 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent {
+  isRecipieBookOpen = true;
+  onNaveTo(isRecipie: boolean) {
+    this.isRecipieBookOpen = isRecipie;
+  }
+}
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,8 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'cookbook-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  @Output('navTo') navEmitter = new EventEmitter<boolean>();
+
+  onRecipie() {
+    this.navEmitter.emit(true);
+  }
+  onShopping() {
+    this.navEmitter.emit(false);
+  }
+}
