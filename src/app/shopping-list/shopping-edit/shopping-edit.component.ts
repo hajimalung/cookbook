106476b19ad45466550552934e391b5a7603ae65
hajimalung <hajimalungbaba.786@gmail.com>
2022-02-23 18:22:00 +0530

    shopping items can be added

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
-  styleUrls: ['./shopping-edit.component.css']
+  styleUrls: ['./shopping-edit.component.css'],
 })
 export class ShoppingEditComponent implements OnInit {
+  @Output('newItemAdded') newItemEmitter = new EventEmitter<Ingredient>();
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  addNewItem(nameInput: HTMLInputElement, amountInput: HTMLInputElement) {
+    this.newItemEmitter.emit(
+      new Ingredient(nameInput.value, Number(amountInput.value))
+    );
   }
-
 }
